6ef1789e63ca1b998f8163893ad3d51780b2456e
camilapessoa <camilabentespessoa@gmail.com>
2020-09-11 17:19:10 -0300

    adicionando tabela

--- projeto/index.js
+++ projeto/index.js
@@ -3,3 +3,14 @@ console.log('     Projeto Carrinho de Compras     ')
 console.log('--------------------------------------')
 
 const db = require('./database')
+//const readline = require('readline-sync')
+
+console.log(db);
+//console.table(db) - verificar comando para listar os produtos como tabela ***
+
+const{produtos}= db
+produtos.sort ((a, b) => b.preco - a.preco)
+
+console.table(produtos)
+
+
