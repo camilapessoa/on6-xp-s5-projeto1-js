eb7e7cdf77021941b0c7a974ff48f59456c82254
camilapessoa <camilabentespessoa@gmail.com>
2020-09-11 18:49:07 -0300

    adicionando array

--- projeto/index.js
+++ projeto/index.js
@@ -3,7 +3,6 @@ console.log('     Projeto Carrinho de Compras     ')
 console.log('--------------------------------------')
 
 const db = require('./database')
-//const readline = require('readline-sync')
 
 console.log(db);
 //console.table(db) - verificar comando para listar os produtos como tabela ***
@@ -13,4 +12,11 @@ produtos.sort ((a, b) => b.preco - a.preco)
 
 console.table(produtos)
 
+const readline = require('readline-sync')
 
+const array = new Array()
+
+let IDproduto
+let procurarID
+let itensCompras
+let promoCode
